4b3b46a17b34c5c7107fbd55e663efac873121b0
Lars <45080708+lars-re@users.noreply.github.com>
2022-06-24 17:13:29 +0200

    chore: bugfixes, sort posts by time, minor improvements

--- fetcher.js
+++ fetcher.js
@@ -6,11 +6,13 @@ const rateLimit = require('axios-rate-limit');
 
 const http = rateLimit(axios.create(), { maxRequests: 2, perMilliseconds: 1000, maxRPS: 2 }) // throttle webhook requests. Works fine.
  
+
 const makeHash = (data) => {
     const dt = new Date();
     data["date"] = (dt.getDay()+1).toString() + "." + (dt.getMonth()+1).toString();
     return crypto.createHash('md5').update(JSON.stringify(data)).digest('hex') // bonk, very ugly I know.
 }
+
 
 const send_webhook_embed = async(data) => {
     let embed = {
@@ -22,26 +24,47 @@ const send_webhook_embed = async(data) => {
         },
         fields: [{
                 name: "Abfahrt",
-                value: `${data.plan_departure}\n${data.actual_departure !== data.plan_departure ? "\n⌚⚠ **Verspätung**: "+data.actual_departure + " (**+"+ data.delay_mins.toString()+"**)" : ""}`,
+                value: data.plan_departure === data.actual_departure ? data.plan_departure : `⌚⚠ **Verspätet**: ${data.actual_departure} (**+${data.delay_mins.toString()}**)\n\n*Ursprünglich: ${data.plan_departure}*`,
                 inline: true
             },
             {
                 name: "Gleis",
                 value: data.platform,
                 inline: true
-            },
-            {
-                name: "Meldungen", // senk you for traeveling
-                value: data.messages.delay + "\n\n" + data.messages.qos + "\n\n" + data.messages.zugI,
-                inline: false
             }
         ]
 
 
     }
-    if(!data.messages.delay && !data.messages.qos && !data.messages.zugI) {
-        embed.fields.pop()
+    if(data.messages && data.messages.join("\n").length < 1000) {
+        embed.fields.push({
+            name: "Meldungen", // senk you for traeveling
+            value: data.messages.join("\n"),
+            inline: false
+        })
+    } else if(data.messages) {
+       let out = []
+       let curr = ""
+       data.messages.forEach(el => {
+        if(curr.length+el.length > 1000) {
+            out.push(curr);
+            curr = el + "\n"
+        } else {
+            curr += el + "\n"
+        }
+       })
+       if(curr.length>1) {
+        out.push(curr.substring(0,curr.length-1))
+       }
+       for(let i = 0; i < out.length; i++) {
+        embed.fields.push({
+            name: i === 0 ? "Meldungen" : "_ _",
+            value: out[i],
+            inline: false
+        })
+       }
     }
+
     let msgid;
     try {
         const msg = await http.post(data.start_bf.includes(process.env.HOME_VARIANT) ? process.env.HOME_WH+"?wait=true" : process.env.WORK_WH+"?wait=true", { // dont code like me kids mkay
@@ -54,7 +77,7 @@ const send_webhook_embed = async(data) => {
     } catch (e) {
         console.error(e)
     }
-    return ((data.start_bf.includes(process.env.HOME_VARIANT) ? process.env.HOME_WH : process.env.WORK_WH)+"/messages/"+msgid) ?? "1234567899"; // lol this really needs refactor
+    return [((data.start_bf.includes(process.env.HOME_VARIANT) ? process.env.HOME_WH : process.env.WORK_WH)+"/messages/"+msgid) ?? "1234567899",embed] // lol this really needs refactor
 
 }
 const fetch_station = async(station_id, train_names) => {
@@ -80,11 +103,11 @@ const fetch_station = async(station_id, train_names) => {
         
         let [delM, accM, zugI] = [train.messages.delay, train.messages.qos, train.messages.him];
         const parseMsg = (m) => {return m.map((m) => {
-            const datx = new Date(m.timestamp) // dont look at how I format the dates. Also the month/day display is broken.
-            return (datx.getDay()+1).toString()+ "." + (datx.getMonth()+1).toString()+ ". "+datx.getHours().toString().padStart(2, '0') + ":" + datx.getMinutes().toString().padStart(2, '0')+ ": " + m.text
+            return "▫️ "+ m.text.trim()
         })};
-    
-        let messages = {delay: parseMsg(delM).join("\n"), qos: parseMsg(accM).join("\n"), zugI: parseMsg(zugI).join("\n")};
+        let messages = [...parseMsg(delM), ...parseMsg(accM), ...parseMsg(zugI)]
+
+        
         lst.push({
             name: train.train.name,
             destination: train.destination,
@@ -112,49 +135,115 @@ const poller = async() => {
         try {
             let [stat1, stat2] = await Promise.all([fetch_station(process.env.HOME_ID, process.env.TRAINS.split(",")),fetch_station(process.env.WORK_ID, process.env.TRAINS.split(","))]); // very ugly code
             let [stat1_hash, stat2_hash] = [stat1.map(tt => makeHash(tt)), stat2.map(tt => makeHash(tt))]; // tracking changes by hashing objects is very unprofessional.
-            let current_hashes = Object.values(traindata).map(tt => tt.hash);
-            for(let i = 0; i < stat1_hash.length; i++) {
+            let current_hashes = Object.values(traindata).map(tt => tt.relevant.hash);
+            for(let i = stat1_hash.length-1; i >= 0; i--) {
                 if(!current_hashes.includes(stat1_hash[i])) {
     
-                    const wh_out = await send_webhook_embed(stat1[i]);
+                    const [wh_out, sentEmb] = await send_webhook_embed(stat1[i]);
                     if(traindata[stat1[i].trainid]) {
                         try {
                             await http.delete(traindata[stat1[i].trainid].del_url);
+                            
                         } catch(err){}
+                        delete train[stat1[i].trainid]
                         
                     }
                     traindata[stat1[i].trainid] = {
+                        relevant: {
+                            id: stat1[i].trainid,
+                            hash: stat1_hash[i],
+                            actual: stat1[i].actual_unix,
+                            home: true
+                        },
                         del_url: wh_out,
-                        hash: stat1_hash[i],
-                        delete_at: stat1[i].actual_unix + 60 * 15 // bug: if a train has 20 mins delay, it's gone.
+                        posted: Date.now(),
+                        emb_data: sentEmb,
+
                     }
                 }
             }
-            for(let i = 0; i < stat2_hash.length; i++) { 
+            for(let i = stat2_hash.length -1; i >= 0; i--) { 
                 if(!current_hashes.includes(stat2_hash[i])) {
     
-                    const wh_out = await send_webhook_embed(stat2[i]);
+                    const [wh_out, sentEmb] = await send_webhook_embed(stat2[i]);
                     if(traindata[stat2[i].trainid]) {
                         try {
                             await http.delete(traindata[stat2[i].trainid].del_url);
+                            
                         } catch(err){}
-                        
+                        delete traindata[stat2[i].trainid]
                     }
                     traindata[stat2[i].trainid] = {
+                        relevant: {
+                            id: stat2[i].trainid,
+                            hash: stat2_hash[i],
+                            actual: stat2[i].actual_unix,
+                            home: false
+                        },
                         del_url: wh_out,
-                        hash: stat2_hash[i],
-                        delete_at: stat2[i].actual_unix + 60 * 15 // bug: if a train has 20 mins delay, it's gone.
+                        posted: Date.now(),
+                        emb_data: sentEmb,
+
                     }
                 }
             }
             const curt = Date.now() / 1000;
-            Object.values(traindata).filter(tt => tt.delete_at < curt).forEach(tt => {
+            Object.values(traindata).filter(tt => (tt.relevant.actual + 60 * 15) < curt).forEach(tt => { // bug: if a train has 20 mins delay, it's gone.
                 try {
+                    delete traindata[tt.id]
                     http.delete(tt.del_url);
                 } catch(err){}
             })
-            traindata = Object.fromEntries(Object.entries(traindata).filter(tt => tt[1].delete_at >= curt));
-    
+
+            const bubbleSort = (inputArr) => {
+                let len = inputArr.length;
+                let checked;
+                let ops = []
+                do {
+                    checked = false;
+                    for (let i = 0; i < len -1; i++) {
+                        if (inputArr[i].relevant.actual > inputArr[i + 1].relevant.actual) {
+                            let tmp = inputArr[i];
+                            ops.push([inputArr[i].relevant.id, inputArr[i+1].relevant.id])
+                            inputArr[i] = inputArr[i + 1];
+                            inputArr[i + 1] = tmp;
+                            checked = true;
+                        }
+                    }
+                } while (checked);
+                return ops;
+            };
+            const sortHome = bubbleSort(Object.values(traindata).filter(o => o.relevant.home === true).sort((a,b) => b.posted - a.posted))
+            console.log(sortHome)
+            for(let i = 0; i < sortHome.length; i++) {
+                const itm = sortHome[i]
+                const old_set = traindata[itm[0]]
+
+                await http.patch(traindata[itm[0]].del_url, {embeds: [traindata[itm[1]].emb_data]})
+                await http.patch(traindata[itm[1]].del_url, {embeds: [traindata[itm[0]].emb_data]})
+                traindata[itm[0]].del_url = traindata[itm[1]].del_url
+                traindata[itm[0]].posted = traindata[itm[1]].posted
+                traindata[itm[1]].posted = old_set.posted
+                traindata[itm[1]].del_url = old_set.del_url
+
+                
+            }
+            const sortWork = bubbleSort(Object.values(traindata).filter(o => o.relevant.home === false).sort((a,b) => b.posted - a.posted))
+            console.log(sortWork)
+            for(let i = 0; i < sortWork.length; i++) {
+                const itm = sortWork[i]
+                const old_set = traindata[itm[0]]
+
+                await http.patch(traindata[itm[0]].del_url, {embeds: [traindata[itm[1]].emb_data]})
+                await http.patch(traindata[itm[1]].del_url, {embeds: [traindata[itm[0]].emb_data]})
+                traindata[itm[0]].del_url = traindata[itm[1]].del_url
+                traindata[itm[0]].posted = traindata[itm[1]].posted
+                traindata[itm[1]].posted = old_set.posted
+                traindata[itm[1]].del_url = old_set.del_url
+
+                
+            }     
+
         } catch(err) {
             console.error(err)
         }
